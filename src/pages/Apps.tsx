60a920753ceb59b531f70add8862783523f132f0
DezS <workingdezs@gmail.com>
2024-09-23 15:14:06 +0700

    Added Card and change it to inline-block

--- src/pages/Apps.tsx
+++ src/pages/Apps.tsx
@@ -1,10 +1,14 @@
 import GreyCenteredFlat from "../components/GreyCenteredFlat";
+import Card from "../components/Card";
 import "../css/Apps.css";
+
 const Apps = () => {
   return (
     <div id="apps-page-root">
-      <GreyCenteredFlat>hehe</GreyCenteredFlat>
-      <div id="apps-page-content"></div>
+      <GreyCenteredFlat id="greyTray">
+        <Card></Card>
+        <Card></Card>
+      </GreyCenteredFlat>
     </div>
   );
 };
